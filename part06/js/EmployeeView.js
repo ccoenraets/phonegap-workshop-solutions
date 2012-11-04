f7a6cd684580a1fae5f99e8e29ae4f6eb1c806b4
Christophe Coenraets <ccoenraets@gmail.com>
2012-11-04 08:49:17 -0500

    Reorganized directories

---
+++ part06/js/EmployeeView.js
@@ -0,0 +1,16 @@
+var EmployeeView = function(employee) {
+
+    this.initialize = function() {
+        this.el = $('<div/>');
+    };
+
+    this.render = function() {
+        this.el.html(EmployeeView.template(employee));
+        return this;
+    };
+
+    this.initialize();
+
+ }
+
+EmployeeView.template = Handlebars.compile($("#employee-tpl").html());
